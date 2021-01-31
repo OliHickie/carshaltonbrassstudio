8a7317d7e77c6e95490ecd89c7e11974e5058f66
OliHickie <oli.h@hotmail.co.uk>
2021-01-31 17:02:51 +0000

    twitter button js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
-$(document).ready(function(){
-$("#twitter-btn").click(function(){
-    $("#twitter-feed").slideUp()
-});
-});
+// $(document).ready(function () {
+//     $(".twitter-button").click(function () {
+//         $(".twitter-feed").slideToggle();
+//     });
+// });
+
+function myFunction(){
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
