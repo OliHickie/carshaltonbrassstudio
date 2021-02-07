25f825c0ca6e52297e9aa9c5ce02c0de42581b66
OliHickie <oli.h@hotmail.co.uk>
2021-02-07 21:40:38 +0000

    form added on play

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
-    function twitterFunction(){
+function twitterFunction() {
     var x = document.getElementById("twitter-feed");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
+}
+
+function openForm() {
+    document.getElementById("form-display").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("form-display").style.display = "none";
 }
