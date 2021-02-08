4da680a2ffbf3dcb215176189687ed796725e169
OliHickie <oli.h@hotmail.co.uk>
2021-02-08 19:06:47 +0000

    added closeform and alert functions to signup

--- assets/js/signup-form.js
+++ assets/js/signup-form.js
@@ -6,7 +6,7 @@ function closeForm() {
     document.getElementById("form-display").style.display = "none";
 }
 
-function sendMail(contactForm) {
+function sendMail(contactForm){
     emailjs.send("service_z6cxmcd", "template_kz2p2kk", {
         "parent_name": contactForm.parentName.value,
         "student_name": contactForm.studentName.value,
@@ -14,11 +14,17 @@ function sendMail(contactForm) {
         "instrument": contactForm.instrument.value,
         "grade": contactForm.grade.value
     })
-    .then (
+    .then(
         function(response) {
             console.log("SUCCESS", response);
         },
         function(error) {
             console.log("FAILED", error);
-        });
+        },
+        alert("Thank you for applying, we will be in touch shortly!"),
+        closeForm()
+
+        );
+        
+        return false;
 }
