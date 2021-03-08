0e6060e8899092ebec17bfd75073e0fab322b18a
OliHickie <oli.h@hotmail.co.uk>
2021-03-08 18:47:39 +0000

    added text to play, edited photo display

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,37 @@ function twitterFunction() {
 
 function playPhotos() {
     var photos = [
-
+        '<img src="assets/images/play/bbp-1.jpg" alt="picOne">',
+        '<img src="assets/images/play/bbp-2.jpg" alt="picOne">',
+        '<img src="assets/images/play/bbp-3.jpg" alt="picOne">',
+        '<img src="assets/images/play/bbp-4.jpg" alt="picOne">',
+        '<img src="assets/images/play/bbp-5.jpg" alt="picOne">',
+        '<img src="assets/images/play/bbp-6.jpg" alt="picOne">'
     ]
+
+    function setPhotos(id, initialPhoto) {
+        let photoCounter = initialPhoto;
+
+        $(id).append(photos[photos.length - (initialPhoto -1)]);
+
+        function cyclePhotos() {
+            ++photoCounter;
+            if (photoCounter >= photos.length) {
+                photoCounter = 0;
+            }
+            // Add photos to html 
+            $(id).fadeOut(1000, function () {
+                $(id).fadeIn(1000).html(photos[photoCounter]);
+            });
+        }
+        // Set Interval between photos 
+        setInterval(cyclePhotos, 8000);
+    }
+
+    setPhotos("#picOne", 2);
+    setPhotos("#picTwo", 3);
+    setPhotos("#picThree", 4);
+    setPhotos("#picFour", 5);
 }
 
 function blink_text() {
@@ -22,5 +51,5 @@ setInterval(blink_text, 1000);
 
 
 $(document).ready(function () {
-    
+    playPhotos()
 });
