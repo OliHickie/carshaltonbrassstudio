908dc957726ca3f2283b773a1dfa4aa0a96e5633
OliHickie <oli.h@hotmail.co.uk>
2021-03-11 14:59:27 +0000

    finalised img cycle and positioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,18 +9,23 @@ function twitterFunction() {
 
 function playPhotos() {
     var photos = [
-        '<img src="assets/images/play/bbp-1.jpg" alt="picOne">',
-        '<img src="assets/images/play/bbp-2.jpg" alt="picOne">',
-        '<img src="assets/images/play/bbp-3.jpg" alt="picOne">',
-        '<img src="assets/images/play/bbp-4.jpg" alt="picOne">',
-        '<img src="assets/images/play/bbp-5.jpg" alt="picOne">',
-        '<img src="assets/images/play/bbp-6.jpg" alt="picOne">'
+        '<img src="assets/images/play/bbp-1.jpg" alt="big brass play along image1">',
+        '<img src="assets/images/play/bbp-2.jpg" alt="big brass play along image 2">',
+        '<img src="assets/images/play/bbp-3.jpg" alt="big brass play along image 3">',
+        '<img src="assets/images/play/bbp-4.jpg" alt="big brass play along image 4">',
+        '<img src="assets/images/play/bbp-5.jpg" alt="big brass play along image 5">',
+        '<img src="assets/images/play/bbp-6.jpg" alt="big brass play along image 6">',
+        '<img src="assets/images/play/bbp-7.jpg" alt="big brass play along image 7">',
+        '<img src="assets/images/play/bbp-8.jpg" alt="big brass play along image 8">',
+        '<img src="assets/images/play/bbp-9.jpg" alt="big brass play along image 9">',
+        '<img src="assets/images/play/bbp-10.jpg" alt="big brass play along image 10">',
+        '<img src="assets/images/play/bbp-11.jpg" alt="big brass play along image 11">'
     ]
 
     function setPhotos(id, initialPhoto) {
         let photoCounter = initialPhoto;
 
-        $(id).append(photos[photos.length - (initialPhoto -1)]);
+        // $(id).append(photos[photos.length - (initialPhoto -1)]);
 
         function cyclePhotos() {
             ++photoCounter;
@@ -33,13 +38,19 @@ function playPhotos() {
             });
         }
         // Set Interval between photos 
-        setInterval(cyclePhotos, 8000);
+        setInterval(cyclePhotos, 12000);
     }
 
-    setPhotos("#picOne", 2);
-    setPhotos("#picTwo", 3);
-    setPhotos("#picThree", 4);
-    setPhotos("#picFour", 5);
+    setPhotos("#picOne", 1);
+    setTimeout(function(){ 
+        setPhotos("#picTwo", 8); 
+    }, 6000);
+    setTimeout(function(){ 
+        setPhotos("#picThree", 5); 
+    }, 9000);
+    setTimeout(function(){ 
+        setPhotos("#picFour", 10); 
+    }, 3000);
 }
 
 function blink_text() {
@@ -51,5 +62,9 @@ setInterval(blink_text, 1000);
 
 
 $(document).ready(function () {
+    $("#picOne").append('<img src="assets/images/play/bbp-1.jpg" alt="big brass play along image1">');
+    $("#picTwo").append('<img src="assets/images/play/bbp-7.jpg" alt="big brass play along image1">');
+    $("#picThree").append('<img src="assets/images/play/bbp-4.jpg" alt="big brass play along image1">');
+    $("#picFour").append('<img src="assets/images/play/bbp-9.jpg" alt="big brass play along image1">');
     playPhotos()
 });
