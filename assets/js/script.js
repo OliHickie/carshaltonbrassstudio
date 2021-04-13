d3b37bed4db80addd2ccec3868bbc70c50f5e031
OliHickie <oli.h@hotmail.co.uk>
2021-04-13 10:28:07 +0000

    add active class to navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,11 @@
 function twitterFunction() {
-    var x = document.getElementById("twitter-feed");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+  var x = document.getElementById("twitter-feed");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 
-    $(".fa-chevron-circle-down").toggleClass("flip-icon")
+  $(".fa-chevron-circle-down").toggleClass("flip-icon");
 }
-
-
-
-// function blink_text() {
-//     $('.fa-chevron-down').fadeOut(1000);
-//     $('.fa-chevron-down').fadeIn(1000);
-// }
-// setInterval(blink_text, 1000);
-
-
 
