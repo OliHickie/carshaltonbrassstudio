b215dc22365630316cc35e0bb1fa47813d5489c8
OliHickie <oli.h@hotmail.co.uk>
2021-10-11 15:07:00 +0000

    All contact goes through contact form

--- assets/js/signup-form.js
+++ assets/js/signup-form.js
@@ -1,18 +1,16 @@
-function openForm() {
-    document.getElementById("form-display").style.display = "block";
-}
+// function openForm() {
+//     document.getElementById("form-display").style.display = "block";
+// }
 
-function closeForm() {
-    document.getElementById("form-display").style.display = "none";
-}
+// function closeForm() {
+//     document.getElementById("form-display").style.display = "none";
+// }
 
 function sendMail(contactForm){
     emailjs.send("service_z6cxmcd", "template_kz2p2kk", {
         "name": contactForm.name.value,
         "email": contactForm.email.value,
-        "instrument": contactForm.instrument.value,
-        "grade": contactForm.grade.value,
-        "playalong": contactForm.playalong_select.value
+        "message": contactForm.message.value
     })
     .then(
         function(response) {
